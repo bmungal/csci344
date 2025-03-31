5c413675b2df32eaab5d1fde7336a678621441e1
bmungal <bmungal@unca.edu>
2025-03-31 13:31:59 -0400

    trying to update html

--- Quiz 2/quiz02a/task02/main.js
+++ Quiz 2/quiz02a/task02/main.js
@@ -1,9 +1,16 @@
 // your function here
+//const trackDiv = document.getElementById("track-list");
+
 function addThing() {
   const track = `
     <div>
         <h3>When the Sun Hits<h/3>
         <p>Slowdive</p>
         <p>Souvlaki</p>
+    </div>
     `;
 }
+
+const button = document.getElementById("track-list");
+
+button.addEventListener("click", addThing());
---
+++ tutorials/tutorial08/app.jsx
@@ -0,0 +1,56 @@
+// at the top:
+
+import { Image } from 'antd';
+import React from 'react';
+
+function App() {
+    <main>
+
+<h2 className="font-Comfortaa my-4 font-bold text-xl">
+     Photo Gallery
+ </h2>
+ <div className="flex flex-wrap content-start">
+     <Image
+         src="https://picsum.photos/600/600?id=1"
+         width={200}
+     />
+     <Image
+         src="https://picsum.photos/600/600?id=2"
+         width={200}
+     />
+     <Image
+         src="https://picsum.photos/600/600?id=3"
+         width={200}
+     />
+     <Image
+         src="https://picsum.photos/600/600?id=4"
+         width={200}
+     />
+     <Image
+         src="https://picsum.photos/600/600?id=5"
+         width={200}
+     />
+     <Image
+         src="https://picsum.photos/600/600?id=6"
+         width={200}
+     />
+     <Image
+         src="https://picsum.photos/600/600?id=7"
+         width={200}
+     />
+     <Image
+         src="https://picsum.photos/600/600?id=8"
+         width={200}
+     />
+     <Image
+         src="https://picsum.photos/600/600?id=9"
+         width={200}
+     />
+     <Image
+         src="https://picsum.photos/600/600?id=10"
+         width={200}
+     />
+ </div>
+</main>
+<
+}
